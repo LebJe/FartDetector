e9d534192a48e597548336749b7f547907dfa08c
LebJe <51171427+LebJe@users.noreply.github.com>
2020-05-31 16:56:57 -0400

    commit

--- Sources/FartDetector/main.swift
+++ Sources/FartDetector/main.swift
@@ -2,15 +2,15 @@ import Foundation
 import SwiftyGPIO
 
 let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi3)
-var gp = gpios[.P1]!
+var gp = gpios[.P2]!
 
 gp.direction = .OUT
 
 while true {
-	gp.value = 1
+	gp.value = 0
 	print("ON")
 	sleep(2)
-	gp.value = 0
+	gp.value = 1
 	print("OFF")
 	sleep(2)
 }
